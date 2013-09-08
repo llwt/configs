86c02c065a8946d8f72b5a14ce1fa49b4170c7f1
Steven Nance <steven@devtrw.com>
2013-09-08 03:17:24 -0700

    Restructure

--- .bashrc
+++
@@ -1,5 +0,0 @@
-# Add RVM to PATH for scripting
-PATH="${HOME}/.rvm/bin:${PATH}"
-
-# Start up powerline
-. /usr/local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+# Initial version stolen from thoughtbot/dotfiles/install.sh
+
+for name in dotfiles/*; do
+
+  # Strip "dotfiles/" out of target file name
+  targetName=${name#dotfiles/}
+  target="$HOME/.${targetName}"
+
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [ "$targetName" != 'install.sh' ] && [ "$targetName" != 'README.md' ]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
+
+# Link zsh theme
+ln -sf ./zsh/solarized-powerline.zsh-theme ~/.oh-my-zsh/themes/solarized-powerline.zsh-theme
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vimrc.before +BundleInstall +qa
