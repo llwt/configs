e9b02c3980d361c1fdb4eeba10e434e68f0ee25f
Steven Nance <steven@devtrw.com>
2016-06-06 12:16:28 -0700

    misc updates

--- utils/move-videos-to-workstation.sh
+++ utils/move-videos-to-workstation.sh
@@ -4,7 +4,7 @@
 set -e
 
 LOCAL_DIR=~/Movies
-WORKSTATION_DIR="steven@10.3.2.2:/Volumes/proc-space/_todo/video"
+WORKSTATION_DIR="steven@10.3.2.2:/Volumes/proc-space/_todo/synced"
 
 
-rsync -avz --progress --remove-source-files -e ssh ${LOCAL_DIR} ${WORKSTATION_DIR}
+rsync -avz --progress --remove-source-files --bwlimit=7500 -e ssh ${LOCAL_DIR} ${WORKSTATION_DIR}
